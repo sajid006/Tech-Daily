6290172fc69367c49285d76f91c3864226cd2542
Sajid Hasan <sajid.hasan@cefalo.com>
2022-11-01 12:12:55 +0600

    apiUrl changed

--- src/utils/ApiUrl.js
+++ src/utils/ApiUrl.js
@@ -1,3 +1,3 @@
-//const apiUrl = "https://techdailybackend.azurewebsites.net/api/v1/";
-const apiUrl = "http://localhost:3000/api/v1/"
+const apiUrl = "https://techdailybackend.azurewebsites.net/api/v1/";
+//const apiUrl = "http://localhost:3000/api/v1/"
 export default apiUrl;
