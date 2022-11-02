64344cebec6b7179f704597cf4c59df1f0b07fdb
Sajid Hasan <sajid.hasan@cefalo.com>
2022-11-02 10:37:43 +0600

    solved sidebar

---
+++ src/lib/NavData.js
@@ -0,0 +1,23 @@
+import { Edit, Info, Person } from '@mui/icons-material';
+import React from 'react';
+export const NavData = [
+    {
+        id: 0,
+        icon: <Edit/>,
+        text: "Add Story",
+        link: "/stories/new"
+    },
+    {
+        id: 1,
+        icon: <Person/>,
+        text: "Author Directory",
+        link: "/users"
+    },
+    {
+        id: 2,
+        icon: <Info/>,
+        text: "About Us",
+        link: "/about"
+    }
+    
+]
