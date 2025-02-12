c38ee6c5ecaf5460f621baae221db3e23d5cfd79
Sajid Hasan <sajid@kelda.no>
2025-02-12 14:46:29 +0600

    Receive api url from env file

--- src/utils/ApiUrl.js
+++ src/utils/ApiUrl.js
@@ -1,4 +1,2 @@
-const apiUrl = "https://techdailybackend.azurewebsites.net/api/v1/";
-//const apiUrl = "http://localhost:3000/api/v1/"
-//const apiUrl = "http://192.168.99.100:3000/api/v1/";
+const apiUrl = process.env.API_URL;
 export default apiUrl;
